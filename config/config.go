f82fc93d72c4b102888113b2dd45b54b49201c56
keith.hand <43491549+keithhand@users.noreply.github.com>
2024-07-01 11:12:15 -0500

    refactor(config): passes logger directly to function calls

--- config/config.go
+++ config/config.go
@@ -6,51 +6,55 @@ import (
 	"strconv"
 )
 
-type Config struct {
-	Server
-	GitHubApi
-}
-
-var log logger
-
-type logger interface {
+type Logger interface {
 	Debug(string, ...any)
 	Error(string, ...any)
 }
 
-func New(lgr logger) *Config {
-	log = lgr
-	return &Config{
-		GitHubApi: GitHubApi{
-			profile: newEnv("GH_PROFILE", "keithhand"),
-		},
-		Server: Server{
-			port: newEnv("SERVER_PORT", 8080),
-		},
-	}
+type Config struct {
+	Server
+	GitHubApi
 }
 
 type Server struct {
 	port int
 }
 
-func (cfg Server) Port() int {
-	return cfg.port
-}
-
 type GitHubApi struct {
+	mockApi bool
 	profile string
 }
 
-func (cfg GitHubApi) GHProfile() string {
+func New(lgr Logger) *Config {
+	return &Config{
+		GitHubApi: GitHubApi{
+			mockApi: newEnv("MOCK_API", true, lgr),
+			profile: newEnv("GH_PROFILE", "keithhand", lgr),
+		},
+		Server: Server{
+			port: newEnv("SERVER_PORT", 8080, lgr),
+		},
+	}
+}
+
+func (cfg Config) Port() int {
+	return cfg.port
+}
+
+func (cfg GitHubApi) MockApi() bool {
+	return cfg.mockApi
+}
+
+func (cfg GitHubApi) GhProfile() string {
 	return cfg.profile
 }
 
 type EnvTypes interface {
-	int | string
+	int | string | bool
 }
 
-func newEnv[T EnvTypes](key string, def T) T {
+func newEnv[T EnvTypes](key string, def T, log Logger) T {
+	fbMsg := "using default:"
 	val := def
 	defer func() {
 		log.Debug("finished configuring env:", key, val)
@@ -58,12 +62,14 @@ func newEnv[T EnvTypes](key string, def T) T {
 
 	env := os.Getenv(key)
 	if env == "" {
-		log.Debug("env value not set, falling back to default", key, val)
+		log.Debug(fmt.Sprintf("env value not set, %s", fbMsg), key, val)
 		return val
 	}
 
 	if err := setValueFromEnv(&val, env); err != nil {
-		log.Error(fmt.Errorf("config: setting %s: %w", key, err).Error())
+		log.Error(
+			fmt.Errorf("config: setting %s %w, %s", key, err, fbMsg).Error(),
+			key, val)
 	}
 
 	return val
@@ -78,12 +84,19 @@ func setValueFromEnv[T EnvTypes](val *T, env string) error {
 	case *int:
 		ival, err := strconv.Atoi(env)
 		if err != nil {
-			return fmt.Errorf("converting %s to %T", env, *val)
+			return fmt.Errorf("converting '%s' to %T", env, *val)
 		}
 		*vp = ival
 
+	case *bool:
+		bval, err := strconv.ParseBool(env)
+		if err != nil {
+			return fmt.Errorf("converting '%s' to %T", env, *val)
+		}
+		*vp = bval
+
 	default:
-		return fmt.Errorf("type not found: %T expected, got %s", *val, env)
+		return fmt.Errorf("type not found: %T expected, got '%s'", *val, env)
 	}
 	return nil
 }
